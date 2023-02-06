2ee72abf6fd73b12624f7048ae71879847dc53e7
Louis <louis.vanden.eede@icloud.com>
2023-02-06 22:01:49 +0100

    suppresion de verif.c, car plus logique de mettre les fonctions dans game_utils

--- game_utils.c
+++ game_utils.c
@@ -34,3 +34,27 @@ void ft_err_choix(int choix)
         sleep(3);
         system("clear");
 }
+
+int ft_verif_atk(struct item item)
+{
+if  (item.sword == 0)
+        return item.atk = 0;
+else if  (item.sword == 1)
+        return item.atk = 5;
+else if (item.sword == 2)
+        return item.atk = 12;
+else
+        return item.atk = 30;
+}
+
+int ft_verif_def(struct item item)
+{
+if  (item.shield == 0)
+        return item.def = 0;
+else if  (item.shield == 1)
+        return item.def = 5;
+else if (item.shield == 2)
+        return item.def = 12;
+else
+        return item.def = 30;
+}
